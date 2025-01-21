cea8860646bba2374f403d9cf39a53b7239619ab
Pedro Farrath <pedroafarrath@gmail.com>
2025-01-21 13:35:58 -0300

    SQL consult, projection, FindByList

---
+++ src/main/java/com/farrathpedro/dslist/projections/GameMinProjection.java
@@ -0,0 +1,12 @@
+package com.farrathpedro.dslist.projections;
+
+public interface GameMinProjection {
+
+    Long getId();
+    String getTitle();
+    Integer getYear();
+    String getImgUrl();
+    String getShortDescription();
+    String getPosition();
+
+}
--- src/main/java/com/farrathpedro/dslist/services/GameService.java
+++ src/main/java/com/farrathpedro/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.farrathpedro.dslist.services;
 import com.farrathpedro.dslist.dto.GameDTO;
 import com.farrathpedro.dslist.dto.GameMinDTO;
 import com.farrathpedro.dslist.entities.Game;
+import com.farrathpedro.dslist.projections.GameMinProjection;
 import com.farrathpedro.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,13 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
 
     }
